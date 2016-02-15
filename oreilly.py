4e65f99559d92a389e8b5fc903b9c378150dd533
ExperimentalVision <jason@amsdenworld.com>
2016-02-15 13:13:13 -0600

    first commit - learning python exercises

---
+++ oreilly.py
@@ -0,0 +1,90 @@
+
+# run in interpretter to get type
+type(1)
+
+name = "jason"
+"hello " + name
+
+print "hello" + str(1)
+
+print len("hello")
+
+name = "jason"
+print("hello " +name)
+
+# boolean
+
+True
+type(True)
+0 == 0
+"H" in "Hello"
+
+
+if 0 > 2:
+    print("stuff is not happening")
+
+# part 4 conditionals and booleans
+sis = 15
+bro = 12
+if sis > bro:
+    print("sis older")
+else:
+    print("bro older")
+
+x = 1
+print(x > 0 and x < 2)
+
+temp = 32
+if temp > 60 and temp < 75:
+    print("nice")
+else:
+    print("not good")
+
+sis = 15
+bro = 15
+if sis > bro:
+    print("sis older")
+elif sis == bro:
+    print("same age")
+else:
+    print("bro older")
+
+# part 5 lists
+
+# interpreter stuff
+your_list = ["a", "b", "c"]
+
+print type(your_list)
+
+print your_list[0]
+print your_list[1]
+print your_list[2]
+
+your_list.append("d")
+
+print your_list
+print len(your_list)
+
+your_list[0] = "jimmy"
+
+print your_list
+
+print your_list[-1]
+
+# part 6
+
+
+my_name = "jason"
+print my_name
+print my_name[0]
+print "a" in my_name
+
+
+
+
+
+
+
+
+
+
